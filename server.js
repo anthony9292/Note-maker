cb6e3556c03bb879dbc619c652ef1668767f3a88
Anthony Ndegwa <anthon.n24@gmail.com>
2021-05-07 17:28:32 -0400

    update

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ const Routesapi = require("./routes/Routesapi");
 const app = express(); 
 const PORT = process.env.PORT || 9500; 
 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true})); 
+app.use(express.static("public")); 
+app.use("/api", Routesapi); 
+app.use("/", Routeshtml); 
+
+
+app.listen(PORT, () => console.log(`App listing ON PORT: ${PORT}`)); 
