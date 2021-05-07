23191c682e91bb856fca134cfc03b737e9b83298
Anthony Ndegwa <anthon.n24@gmail.com>
2021-05-07 19:05:52 -0400

    routes

--- routes/Routesapi.js
+++ routes/Routesapi.js
@@ -1,5 +1,6 @@
 var db = require("../db/db.json"); 
 var fs = require("fs"); 
+const { stringify } = require("querystring");
 //routes
 
 module.exports = function (app) {  
@@ -20,4 +21,20 @@ module.exports = function (app) {
              res.json(db)
          });
      });
-}         
+     // request to delete api method
+     app.delete("/api/notes/:id", function (req, res) { 
+         var id = req.params. id; 
+       //using splice to delete selected note from db 
+         db.splice(id -1, 1); 
+    
+         db.forEach((obj, i) => { 
+             obj. id = i+ 1; 
+         }); 
+
+         fs.writeFile("./db/db.json", JSON,stringify(db), function () { 
+             res.json(db); 
+
+         });
+
+     }); 
+};      
