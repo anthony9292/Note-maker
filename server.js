862ba03a7de1289b5da45fa0e10d8ba33a66caa1
Anthony Ndegwa <anthon.n24@gmail.com>
2021-05-07 17:51:31 -0400

    update

--- server.js
+++ server.js
@@ -7,4 +7,14 @@ const app = express();
 const PORT= parseInt(process.evn.PORT) || 9600;
 
 
-app
+app.use(express.static('./public')); 
+app.get('/notes', (req, res) => { 
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+}); 
+
+app.get('/api/notes', (req, res) => { 
+    let file = fs.readFileSync('./db/db.json'); 
+    let jsonData =JSON.parse(file); 
+    res.send(jsonData); 
+}); 
+
