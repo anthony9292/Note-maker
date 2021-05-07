9ea22df9ef7c7466f7ee8f72ba998cae5073b7a8
Anthony Ndegwa <anthon.n24@gmail.com>
2021-05-07 17:23:26 -0400

    update

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express"); 
+const Routeshtml = require("./routes/Routeshtml");
+const Routesapi = require("./routes/Routesapi"); 
+
+
+const app = express(); 
+const PORT = process.env.PORT || 9500; 
+
