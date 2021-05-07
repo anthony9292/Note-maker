c8961ae875d5851f6e47a73bf967d71df23c93c0
Anthony Ndegwa <anthon.n24@gmail.com>
2021-05-07 17:19:45 -0400

    update

--- routes/Routeshtml.js
+++ routes/Routeshtml.js
@@ -1,3 +1,14 @@
 const router = require("express").Router(); 
 const Path = require("path"); 
 
+router.get("/notes", function (req, res) { 
+      res.sendFile(path.join(__dirname, "../public/notes.html")); 
+
+ }); 
+
+ router.get("*", function (req, res) { 
+     res.sendFile(path.join(__dirname, "../public/index.html")); 
+ }); 
+
+ module.exports = router; 
+ 
